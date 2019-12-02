f2a0b23f1445dbab7e0bc035dcf6f47cfcb9a45e
Raymond LOUA <louaraym@gmail.com>
2019-12-02 12:19:34 +0100

    Project init

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('article/homepage.html.twig');
+    }
+
+    /**
+     * @Route("/news/{slug}", name="article_show")
+     * @param $slug
+     * @return Response
+     */
+    public function show($slug): Response
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
+            'comments' => $comments,
+        ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     * @param $slug
+     * @param LoggerInterface $logger
+     * @return JsonResponse
+     * @throws \Exception
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger): JsonResponse
+    {
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
+
+        return new JsonResponse(['hearts' => random_int(5, 100)]);
+    }
+}
